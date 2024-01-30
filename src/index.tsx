68ffc0fe7610e7d15eb1de532c8b343f62d8af2f
cryptomagic <153769551+cryptomaggic@users.noreply.github.com>
2024-01-30 18:39:34 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -36,7 +36,7 @@ function Root() {
                 creator={PLATFORM_CREATOR_ADDRESS}
                 games={GAMES}
                 tokens={TOKENS}
-                defaultCreatorFee={00.09}
+                defaultCreatorFee={00.05}
                 defaultJackpotFee={0.001}
               >
                 <App />
